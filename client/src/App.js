8f702a7e3ca8c437c31e747d216af557d88c6dfc
Chaya Otikor <c.otikor@gmail.com>
2021-02-18 17:41:29 -0700

    drizzle able to show account data from ganache

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
+import { newContextComponents } from "@drizzle/react-components";
+const {AccountData} = newContextComponents
 
 function App({drizzle}) {
   return (
@@ -12,7 +14,15 @@ function App({drizzle}) {
             return "Loading...";
           }
 
-          return <h1>Hello World</h1>;
+          return (
+            <AccountData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              accountIndex={0}
+              units="ether"
+              precision={3}
+            />
+          );
         }}
       </DrizzleContext.Consumer>
     </DrizzleContext.Provider>
