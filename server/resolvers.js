a40c19e67e6b1916b33eee698890521f24204527
Chaya Otikor <c.otikor@gmail.com>
2021-02-25 15:06:24 -0700

    contract connected to truffle dev server

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,12 +1,33 @@
-const Web3 = require('web3')
-const artifact = require('../client/src/artifacts/Plurality.json')
+// const {contract} = require('./deployment')
 
-const abi = artifact.abi;
-const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+const Web3 = require("web3");
+const { abi } = require("../client/src/artifacts/Plurality.json");
+const web3 = new Web3(Web3.givenProvider || "http://localhost:9545");
 
-const contract = new web3.eth.Contract(
-  abi,
-  process.env.CONTRACT_ADDRESS
+const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS, {
+  from: process.env.DEFAULT_ACCOUNT,
+});
+let balance = await web3.utils.fromWei(
+  web3.eth.getBalance("0x35104d871e5531042Db2943587eCf9c176c9f323")
 );
+console.log(balance);
 
-console.log(contract)
+module.exports = {
+  getElectionOfficial: async (name) => {
+    try {
+      let official = await contract.methods.electionOfficial().call();
+      return official;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  register: async ({ address }) => {
+    try {
+      let event = await contract.methods.register(address).send();
+      return event;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
--- server/schema.js
+++ server/schema.js
@@ -1,7 +1,16 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
-    type Plurality {
-      totalRegisteredVoters: Int
-    }
-`)
+  type RootQuery {
+    getElectionOfficial: String!
+  }
+
+  type RootMutation {
+    register(address: String!): String
+  }
+
+   schema {
+     query: RootQuery
+     mutation: RootMutation
+ }
+`);
